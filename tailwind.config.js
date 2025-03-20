4182dabf90a2c7a47bf3d5dedb258e08d7da479f
Rosine22 <r.uwineza@alustudent.com>
2025-03-20 02:59:00 -0600

    updates on paragraph

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     "./src/**/*.html" // If you have multiple HTML files inside a 'src' folder
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 };
