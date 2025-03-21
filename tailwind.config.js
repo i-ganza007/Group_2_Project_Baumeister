159d6b5f96976e18e2fce3398d20ff0c3c98004a
Rosine22 <r.uwineza@alustudent.com>
2025-03-21 02:20:23 -0600

    changes on paragraph

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-module.exports = {
-  content: [
-    "./index.html",  // If you have a single index.html
-    "./src/**/*.html" // If you have multiple HTML files inside a 'src' folder
-  ],
-  theme: {
-    extend: {
-    },
-  },
-  plugins: [],
+export const content = [
+  "./index.html", // If you have a single index.html
+  "./src/**/*.html" // If you have multiple HTML files inside a 'src' folder
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
