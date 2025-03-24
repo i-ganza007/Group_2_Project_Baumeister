bfeeb5328452ff1582f97a5e44703cbdfffd58dd
Rosine22 <r.uwineza@alustudent.com>
2025-03-24 05:27:46 -0600

    changes on footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
-
-export const content = [
-  "./index.html", // If you have a single index.html
-  "./src/**/*.html" // If you have multiple HTML files inside a 'src' folder
-];
-export const theme = {
-  extend: {},
+module.exports = {
+  content: [
+    './index.html', // If you have a single index.html
+    './src/**/*.html', // If you have multiple HTML files inside a 'src' folder
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: '#EFECEA',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+      // fontFamily: {
+      //   body: ['Nunito']
+      // }
+    },
+  },
+  plugins: [],
 };
-export const plugins = [];
